11aa227b6e4b18cc4540538fcc3e97db9bb18507
ianshull <shulli@dickinson.edu>
2021-01-18 18:24:01 -0500

    Added form button to welcome screen and started the form

--- app/app/src/main/java/com/example/crimereporter/MainActivity.java
+++ app/app/src/main/java/com/example/crimereporter/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.crimereporter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnForm= findViewById(R.id.btnForm);
+        Intent toForm= new Intent(MainActivity.this, com.example.crimereporter.Form.class);
+        startActivity(toForm);
     }
 }
